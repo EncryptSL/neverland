6df82269842143ab6315fb58856482b16c158771
EncryptSL <Rydlos123@gmail.com>
2023-10-27 18:57:44 +0200

    Push fix

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-    return (
-        <section className="container text-center p-5">
-            <h1>404</h1>
-
-            <p>Tato stránka neexistuje !</p>
-        </section>
-    );
-}
--- pages/api/admins.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const members = require('/json/admins.json')
-  res.status(200).json(members)
-}
