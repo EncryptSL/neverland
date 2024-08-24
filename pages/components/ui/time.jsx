43bf6c1982c044fe3a795322cb6d1a3238699bc1
EncryptSL <Rydlos123@gmail.com>
2024-08-24 19:26:23 +0200

    Optimized images and time fix.

--- pages/components/ui/time.jsx
+++ pages/components/ui/time.jsx
@@ -1,6 +1,12 @@
-export default function Time({time}) {
-    const date = new Date(time)
-    const dateTime = new Intl.DateTimeFormat('cs-CZ', {dateStyle: 'full', timeStyle: 'short'}).format(date)
+import { useState, useEffect } from "react";
 
-    return dateTime
+export default function Time({time}) {
+    const [formattedDate, setFormattedDate] = useState(null);
+
+    useEffect(
+      () => setFormattedDate(new Intl.DateTimeFormat('cs-CZ', {dateStyle: 'full', timeStyle: 'short'}).format(new Date(time))),
+      []
+    );
+  
+    return formattedDate;
 }
